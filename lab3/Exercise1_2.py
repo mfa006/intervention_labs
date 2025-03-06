037572f8eb8a13f044a47073778f9e3857caf766
MFA <faran1218@gmail.com>
2025-03-06 09:14:20 +0100

    aff required file

--- lab3/Exercise1_2.py
+++ lab3/Exercise1_2.py
@@ -1,15 +1,15 @@
 from lab4_robotics import * # Includes numpy import
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
-
+import numpy as np
 # Robot model - 3-link manipulator
-d =                             # displacement along Z-axis
-theta =                         # rotation around Z-axis
-alpha =                         # rotation around X-axis
-a =                             # displacement along X-axis
-revolute =                      # flags specifying the type of joints
+d = np.zeros(3)                 # displacement along Z-axis
+theta = np.array([0, np.pi/4, np.pi/4]).reshape(3, 1) # rotation around Z-axis
+alpha = np.zeros(3)             # rotation around X-axis
+a = np.array([0.75, 0.5, 0.5])  # displacement along X-axis
+revolute = np.array([True,True,True])             # flags specifying the type of joints
 robot = Manipulator(d, theta, a, alpha, revolute) # Manipulator object
-
+sigma_d = T[-1][0:2,3].reshape(2,1)
 # Task hierarchy definition
 tasks = [ 
             Position2D("End-effector position", np.array([1.0, 0.5]).reshape(2,1))
@@ -47,7 +47,13 @@ def simulate(t):
     global PPx, PPy
 
     ### Recursive Task-Priority algorithm
+    T = kinematics(d, q.flatten(), a, alpha)
+    J = jacobian(T, revolute)
     # Initialize null-space projector
+    sigma = T[-1][0:2,3].reshape(2,1)# Current position of the end-effector
+    err = sigma_d - sigma    # Error in position
+    Jbar = J[0:2, :]         # Task Jacobian
+    P = np.eye(3) - np.linalg.pinv(Jbar)@Jbar   # Null space projector
     # Initialize output vector (joint velocity)
     # Loop over tasks
         # Update task state
