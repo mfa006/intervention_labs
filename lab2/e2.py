01d5eb9d1e92455a10c087f1d887ee0e1648640d
MFA <faran1218@gmail.com>
2025-03-06 18:28:31 +0100

    format comments in e2.py

--- lab2/e2.py
+++ lab2/e2.py
@@ -87,49 +87,49 @@ def simulate(t):
     if task_flag == 1:
         # CASE 1: End-effector position control as the top hierarchy task
         # TASK 1: End-effector position control
-        sigma1 = T[-1][0:2, -1].reshape(2, 1)                # Current position of the end-effector
+        sigma1 = T[-1][0:2, -1].reshape(2, 1)   # Current position of the end-effector
         err1 = sigma1_d - sigma1                # Error in Cartesian position
         x1_dot = K1 @ err1                      # Velocity for the first task
-        J1 = J[0:2, :]                   # Jacobian of the first task
-        P1 = np.eye(3) - np.linalg.pinv(J1) @ J1                   # Null space projector
+        J1 = J[0:2, :]                          # Jacobian of the first task
+        P1 = np.eye(3) - np.linalg.pinv(J1) @ J1# Null space projector
         
         # TASK 2: Joint position control
-        sigma2 = q[0]               # Current position of joint 1
-        err2 = sigma2_d - sigma2                 # Error in joint position
+        sigma2 = q[0]                           # Current position of joint 1
+        err2 = sigma2_d - sigma2                # Error in joint position
         x2_dot = K2 @ err2                      # Velocity for the second task
-        J2 = np.array([1, 0, 0]).reshape(1, 3)                   # Jacobian of the second task
-        J2bar = J2 @ P1                 # Augmented Jacobian
+        J2 = np.array([1, 0, 0]).reshape(1, 3)  # Jacobian of the second task
+        J2bar = J2 @ P1                         # Augmented Jacobian
         
         # Combining tasks
-        J_DLS1 = DLS(J1, 0.1) # DLS for the first task
-        J_DLS2 = DLS(J2bar, 0.1) # DLS for the second task
-        dq1 = (J_DLS1 @ x1_dot).reshape(3, 1)                  # Velocity for the first task
-        dq12 = dq1 + J_DLS2 @ (x2_dot - J2 @ dq1)                 # Velocity for both tasks
+        J_DLS1 = DLS(J1, 0.1)                   # DLS for the first task
+        J_DLS2 = DLS(J2bar, 0.1)                # DLS for the second task
+        dq1 = (J_DLS1 @ x1_dot).reshape(3, 1)   # Velocity for the first task
+        dq12 = dq1 + J_DLS2 @ (x2_dot - J2 @ dq1)# Velocity for both tasks
   
     elif task_flag == 2:
         # CASE 2: Joint position control as the top hierarchy task
         # TASK 1: Joint position control
-        sigma2 = q[0]               # Current position of joint 1
-        err2 = sigma2_d - sigma2                 # Error in joint position
+        sigma2 = q[0]                           # Current position of joint 1
+        err2 = sigma2_d - sigma2                # Error in joint position
         x2_dot = K2 @ err2                      # Velocity for the second task
-        J1 = np.array([1, 0, 0]).reshape(1, 3)                   # Jacobian of the second task
-        P1 = np.eye(3) - np.linalg.pinv(J1) @ J1                   # Null space projector
+        J1 = np.array([1, 0, 0]).reshape(1, 3)  # Jacobian of the second task
+        P1 = np.eye(3) - np.linalg.pinv(J1) @ J1# Null space projector
 
             
         # TASK 2: End-effector position control
-        sigma1 = T[-1][0:2, -1].reshape(2, 1)                # Current position of the end-effector
+        sigma1 = T[-1][0:2, -1].reshape(2, 1)   # Current position of the end-effector
         err1 = sigma1_d - sigma1                # Error in Cartesian position
         x1_dot = K1 @ err1                      # Velocity for the first task
-        J2 = J[0:2, :]                   # Jacobian of the first task
-        J2bar = J2 @ P1                 # Augmented Jacobian
+        J2 = J[0:2, :]                          # Jacobian of the first task
+        J2bar = J2 @ P1                         # Augmented Jacobian
 
         # Combining tasks
-        J_DLS1 = DLS(J1, 0.1) # DLS for the first task
-        J_DLS2 = DLS(J2bar, 0.1) # DLS for the second task
-        dq2 = (J_DLS1 @ x2_dot).reshape(3, 1)                  # Velocity for the first task
-        dq12 = dq2 + J_DLS2 @ (x1_dot - J1 @ dq2)                 # Velocity for both tasks
+        J_DLS1 = DLS(J1, 0.1)                   # DLS for the first task
+        J_DLS2 = DLS(J2bar, 0.1)                # DLS for the second task
+        dq2 = (J_DLS1 @ x2_dot).reshape(3, 1)   # Velocity for the first task
+        dq12 = dq2 + J_DLS2 @ (x1_dot - J1 @ dq2)# Velocity for both tasks
  
-    q = q + dq12 * dt # Simulation update
+    q = q + dq12 * dt                           # Simulation update
 
     # Update drawing
     PP = robotPoints2D(T)
@@ -141,7 +141,7 @@ def simulate(t):
     # point.set_data(sigma1_d[0], sigma1_d[1]) #goal
     #for error normalization
     err_joint_pos1_norm = np.linalg.norm(err2)  # Joint 1 position error normalized
-    err_ee_pose_norm = np.linalg.norm(err1)  # End-effector position error normalized
+    err_ee_pose_norm = np.linalg.norm(err1)     # End-effector position error normalized
 
     #for plotting
     ee_pose.append(err_ee_pose_norm)
